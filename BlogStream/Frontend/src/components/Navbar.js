60e71f2c035f3e69a8fbcf9568c1638e66ab8891
Vedant Patil <vedantpatil02007@gmail.com>
2024-07-03 15:53:54 +0530

    initial commit

--- BlogStream/Frontend/src/components/Navbar.js
+++ BlogStream/Frontend/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
     <nav className='navbar navbar-expand-md navbar-light bg-light shadow'>
       <div className='container-fluid '>
         <Link to='/' className=' ml-5 mr-5 navbar-brand font-weight-bold text-dark'>
-          Blogging Page
+          Blog Stream
         </Link>
         <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarNav' aria-controls='navbarNav' aria-expanded='false' aria-label='Toggle navigation'>
           <span className='navbar-toggler-icon'></span>
